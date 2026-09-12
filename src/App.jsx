72910ea3e6c7577cba67ab8444ac55fa80271a4b
fadhilhasan <dhaifullahfadhilhasan@gmail.com>
2026-09-12 21:19:52 +0700

    implement postcard

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import FirstVideo from "./sections/FirstVideo";
 import Jason from "./sections/Jason";
 import SecondVideo from "./sections/SecondVideo";
 import Lucia from "./sections/Lucia";
+import PostCard from "./sections/PostCard";
 
 gsap.registerPlugin(ScrollTrigger);
 gsap.ticker.lagSmoothing(0);
@@ -45,6 +46,7 @@ const App = () => {
       <Jason />
       <SecondVideo />
       <Lucia />
+      <PostCard />
     </main>
   );
 };
